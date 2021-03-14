b4f15c68342a24d948a0bf8060a5154801e8c845
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2021-03-14 15:46:06 +0100

    Initial commit

---
+++ src/test/kotlin/com/bme/jnsbbk/oauthserver/OAuthAuthorizationServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bme.jnsbbk.oauthserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OAuthAuthorizationServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
