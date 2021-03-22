77bce6a4d82a61465920507c331e1376d172a71b
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2021-03-22 18:03:41 +0100

    Delete client implementation

--- src/main/kotlin/com/bme/jnsbbk/oauthserver/client/ClientRegistrationController.kt
+++ src/main/kotlin/com/bme/jnsbbk/oauthserver/client/ClientRegistrationController.kt
@@ -2,10 +2,10 @@ package com.bme.jnsbbk.oauthserver.client
 
 import com.bme.jnsbbk.oauthserver.client.validators.ClientValidator
 import com.bme.jnsbbk.oauthserver.exceptions.ApiException
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/register")
@@ -25,16 +25,38 @@ class ClientRegistrationController(
     }
 
     @GetMapping("/{id}")
-    fun getClient(@RequestHeader("Authorization") accessToken: String?,
+    fun getClient(@RequestHeader("Authorization") header: String?,
                   @PathVariable id: String): ResponseEntity<Client> {
-        if (accessToken.isNullOrEmpty() || !accessToken.startsWith("Bearer "))
-            return ResponseEntity(HttpStatus.UNAUTHORIZED)
-
-        val token = accessToken.removePrefix("Bearer ")
-        val result = clientRepository.findById(id)
-        if (result.isEmpty || result.get().registrationAccessToken != token)
+        val result = validateHeaderAndReturnClient(header, id)
+        if (result.isEmpty)
             return ResponseEntity(HttpStatus.UNAUTHORIZED)
 
         return ResponseEntity.ok(result.get())
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteClient(@RequestHeader("Authorization") header: String?,
+                     @PathVariable id: String): ResponseEntity<String> {
+        val result = validateHeaderAndReturnClient(header, id)
+        if (result.isEmpty)
+            return ResponseEntity(HttpStatus.UNAUTHORIZED)
+
+        clientRepository.delete(result.get())
+        return ResponseEntity.noContent().build()
+    }
+
+    private fun validateHeaderAndReturnClient(header: String?, id: String): Optional<Client> {
+        if (header.isNullOrEmpty() || !header.startsWith("Bearer "))
+            return Optional.empty()
+
+        val token = header.removePrefix("Bearer ")
+        if (token.isEmpty())
+            return Optional.empty()
+
+        val result = clientRepository.findById(id)
+        if (result.isEmpty || result.get().registrationAccessToken != token)
+            return Optional.empty()
+
+        return Optional.of(result.get())
     }
 }
