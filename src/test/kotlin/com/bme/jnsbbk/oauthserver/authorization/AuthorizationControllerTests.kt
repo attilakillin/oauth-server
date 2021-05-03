2713da0345c5006e8cdfbe724bff754a4b1632bc
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2021-05-03 19:57:56 +0200

    Authorization & Client registration controller tests

--- src/test/kotlin/com/bme/jnsbbk/oauthserver/authorization/AuthorizationControllerTests.kt
+++ src/test/kotlin/com/bme/jnsbbk/oauthserver/authorization/AuthorizationControllerTests.kt
@@ -1,8 +1,11 @@
 package com.bme.jnsbbk.oauthserver.authorization
 
+import com.bme.jnsbbk.oauthserver.authorization.entities.AuthRequest
 import com.bme.jnsbbk.oauthserver.authorization.validators.AuthValidator
 import com.bme.jnsbbk.oauthserver.client.ClientRepository
+import com.bme.jnsbbk.oauthserver.client.entities.Client
 import com.bme.jnsbbk.oauthserver.jwt.UserJwtHandler
+import com.bme.jnsbbk.oauthserver.utils.RandomString
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import org.hamcrest.Matchers.containsString
@@ -12,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import java.util.*
 
 @WebMvcTest(AuthorizationController::class)
 class AuthorizationControllerTests {
@@ -45,5 +49,30 @@ class AuthorizationControllerTests {
             .perform(get("/authorize").param("redirect_uri", uri))
             .andExpect(status().is3xxRedirection)
             .andExpect(redirectedUrl("$uri?error=$message"))
+    }
+
+    @Test
+    fun authorizationRequested_fillsTemplateCorrectlyOnSuccess() {
+        val name = "test_random_name"
+
+        val client = Client(RandomString.generate()).apply { extraData["client_name"] = name }
+        val request = AuthRequest(
+            clientId = client.id,
+            redirectUri = "",
+            responseType = "",
+            scope = setOf("random", "xyz"),
+            state = null
+        )
+
+        every { authValidator.validateSensitiveOrError(any()) } returns null
+        every { authValidator.validateAdditionalOrError(any()) } returns null
+        every { authValidator.convertToValidRequest(any()) } returns request
+        every { clientRepository.findById(client.id) } returns Optional.of(client)
+
+        mockMvc
+            .perform(get("/authorize"))
+            .andExpect(status().isOk)
+            .andExpect(content().string(containsString("random")))
+            .andExpect(content().string(containsString("xyz")))
     }
 }
