515206e5b5fbfff954c71c208fe3b39b941a56b4
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2021-09-14 18:48:32 +0200

    Fix git not deleting moved files

--- src/main/kotlin/com/bme/jnsbbk/oauthserver/client/ClientRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.bme.jnsbbk.oauthserver.client
-
-import com.bme.jnsbbk.oauthserver.client.entities.Client
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ClientRepository : CrudRepository<Client, String>
--- src/main/kotlin/com/bme/jnsbbk/oauthserver/jwt/RSAKeyRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.bme.jnsbbk.oauthserver.jwt
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface RSAKeyRepository : JpaRepository<RSAKey, String>
