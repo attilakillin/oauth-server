a8927f0c4d2d0ea74e6fb742d15d1847ea5c951e
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2021-11-27 14:12:23 +0100

    Integration test hotfix: Clean resource server DB before each test

--- authorization-server/src/test-integration/kotlin/com/bme/jnsbbk/oauthserver/ResourceUserTokenValidationTests.kt
+++ authorization-server/src/test-integration/kotlin/com/bme/jnsbbk/oauthserver/ResourceUserTokenValidationTests.kt
@@ -4,7 +4,9 @@ package com.bme.jnsbbk.oauthserver
 
 import com.bme.jnsbbk.oauthserver.components.methods.getAndValidateResourceUserToken
 import com.bme.jnsbbk.oauthserver.components.methods.runResourceServerRegistration
+import com.bme.jnsbbk.oauthserver.resource.ResourceServerRepository
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -21,6 +23,12 @@ import org.springframework.test.web.servlet.MockMvc
 @AutoConfigureMockMvc
 class ResourceUserTokenValidationTests {
     @Autowired private lateinit var mvc: MockMvc
+    @Autowired private lateinit var resourceServerRepository: ResourceServerRepository
+
+    @BeforeEach
+    fun cleanResourceServerRegistrations() {
+        resourceServerRepository.deleteAll()
+    }
 
     @Test
     fun resourceUserTokenValidation_testValidation() {
